d7d32604e6644123109096ed1edd9c39b69c972b
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-02-17 21:35:16 +0100

    add CORS options

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const { Event } = require('./classes/Event');
 const moment = require('moment');
 const app = express();
 
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsOptions))
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,

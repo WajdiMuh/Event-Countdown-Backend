95e7410d153c39d301e12eb214d50d2c522c6e5d
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2022-02-17 14:20:10 +0100

    finish adding backend functions

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
+const { Event } = require('./classes/Event');
+const moment = require('moment');
 const app = express();
 
 app.use(express.json());
@@ -11,16 +13,59 @@ const pool = new Pool({
   }
 });
 
-app.get('/db', async (req, res) => {
+app.get('/getallevents', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM events');
-      res.send(result);
+      const result = await client.query('select * from events where date > now() order by date');
+      let eventarray = [];
+      result['rows'].forEach(event => {
+          eventarray.push(new Event(event["id"],event["title"],moment(event["date"]).format("yyyy-MM-DD") + "T" + moment(event["date"]).format("hh:mm:ss")));
+      });
+      res.send(eventarray);
+      client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
   })
+
+app.get('/getlatestevent', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('select * from events where date > now() order by date limit 1');
+      const latesteventjson = result["rows"][0];
+      client.release();
+      if(!latesteventjson){
+        throw "no event";
+      }
+      res.send(new Event(latesteventjson["id"],latesteventjson["title"],moment(latesteventjson["date"]).format("yyyy-MM-DD") + "T" + moment(latesteventjson["date"]).format("hh:mm:ss")));
+    } catch (err) {
+      res.status(400).send(err);
+    }
+})
+
+app.delete('/deleteevent/:id', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      await client.query('delete from events where id = $1',[req.params.id]);
+      client.release();
+      res.send("successful deletion");
+    } catch (err) {
+      res.status(400).send(err);
+    }
+})
+
+app.post('/addevent', async (req, res) => {
+    try {
+      console.log(`insert into events (title,date) values ('${req.body.title}',${moment(req.body.date).format("yyyy-MM-DD") + "T" + moment(req.body.date).format("hh:mm:ss.SSS") + "Z"})`);
+      const client = await pool.connect();
+      await client.query(`insert into events (title,date) values ('${req.body.title}','${moment(req.body.date).format("yyyy-MM-DD") + "T" + moment(req.body.date).format("hh:mm:ss.SSS") + "Z"}')`);
+      client.release();
+      res.send("successful creation");
+    } catch (err) {
+      res.status(400).send(err);
+    }
+})
 
 app.listen(process.env.PORT || 5000,() => {
     console.log('server running');
--- classes/Event.js
+++ classes/Event.js
@@ -1,13 +1,14 @@
 class Event{
-    constructor(title,date){
+    constructor(id,title,date){
+        this.id = id;
         this.title = title;
         this.date = date;
     }
     static fromJson(json) {
-        return new Event(json['title'],json['date']);
+        return new Event(json['id'],json['title'],json['date']);
     }
     toJson() {
-        return {title:this.title,date:this.date};
+        return {id:this.id,title:this.title,date:this.date};
     }
 }
 module.exports = {Event};
